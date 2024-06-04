2d2adef91809fc871d5f64e32db6c80833927d39
jacobamobin <63465655+jacobamobin@users.noreply.github.com>
2024-06-03 23:33:52 -0400

    fixed edit block gui

--- BlockEditGUI.java
+++ BlockEditGUI.java
@@ -107,7 +107,7 @@ public class BlockEditGUI {
                         }
                         break;
                     case "Renewable":
-                        buttonFilePath = "ViewPannelAssets\\Renewable.png";
+                        buttonFilePath = "ViewPannelAssets\\Renewability.png";
                         if (firstButton) {
                             buttonFilePathON = "ViewPannelAssets\\TrueON.png";
                             buttonFilePathOFF = "ViewPannelAssets\\FalseOFF.png";
@@ -127,7 +127,7 @@ public class BlockEditGUI {
                         }
                         break;
                     case "Fire":
-                        buttonFilePath = "ViewPannelAssets\\Fire.png";
+                        buttonFilePath = "ViewPannelAssets\\Flammable.png";
                         if (firstButton) {
                             buttonFilePathON = "ViewPannelAssets\\TrueON.png";
                             buttonFilePathOFF = "ViewPannelAssets\\FalseOFF.png";
@@ -190,27 +190,41 @@ public class BlockEditGUI {
                 blocksArray = sorter.sortBlockParameter("name", "asc"); // Initial sorting when gui open                
                 Font font = new Font("Arial", Font.PLAIN, 60);
                 int numberOfObjects = 0;
+                int sizeOfFrontPng = 0;
+
 
                 for (int i = 0; i < blocksArray.length; i++) {
+                    int y = (scrollDist) + (i * 100) + 100 + 20; // Adjust Y position based on loop iteration (for drwaing name and desc)
+                    Block block = parser.getBlockByName(blocks, blocksArray[i]);
                     if (isWithinButtonRange(mouseX, mouseY, 5, 950, (scrollDist) + (i * 100) + 50, (scrollDist) + (i * 100) + 150)) {
                         BufferedImage highlighted = loadImage("ViewPannelAssets\\BackGroundOfBlockSelected.png");
                         if (highlighted != null) {
-                            g.drawImage(highlighted, 0 - 5, (scrollDist) + (i * 100) - 5, getWidth() + 10, getHeight() + 10, this);
+                            g.drawImage(highlighted, 0 - 5 , (scrollDist) + (i * 100) - 5, 1080 + 10, 720 + 10, this);
                             g.setColor(Color.WHITE);
-                            font = new Font("Arial", Font.PLAIN, 70);
+                            font = new Font("Arial", Font.PLAIN, 45);
+                            sizeOfFrontPng = 70;
+                        }
+                        BufferedImage blockFront = loadImage("object\\" + block.getName() + "\\" + "front.jpg.jpg"); // add front png of block to block
+                        if (blockFront != null) {
+                            g.drawImage(blockFront, 60, y-60, sizeOfFrontPng, sizeOfFrontPng, this);
                         }
 
                     } else {
                         BufferedImage unselected = loadImage("ViewPannelAssets\\BackGroundOfBlockUnselected.png");
                         if (unselected != null) {
-                            g.drawImage(unselected, 0, (scrollDist) + (i * 100), getWidth(), getHeight(), this);
+                            g.drawImage(unselected, 0 , (scrollDist) + (i * 100), 1080, 720, this);
                             g.setColor(Color.LIGHT_GRAY);
-                            font = new Font("Arial", Font.PLAIN, 60);
+                            font = new Font("Arial", Font.PLAIN, 40);
+                            sizeOfFrontPng = 60;
+                        }
+                        BufferedImage blockFront = loadImage("object\\" + block.getName() + "\\" + "front.jpg.jpg"); // add front png of block to block
+                        if (blockFront != null) {
+                            g.drawImage(blockFront, 70, y-50, sizeOfFrontPng, sizeOfFrontPng, this);
                         }
                     }
 
                     g.setFont(font);
-                    int y = (scrollDist) + (i * 100) + 100 + 20 - 10; // Adjust Y position based on loop iteration
+                    y = (scrollDist) + (i * 100) + 100 + 20 - 10; // Adjust Y position based on loop iteration
                     String text = String.valueOf(i + 1);
                     g.drawString(text, 30, y);
                     text = blocksArray[i];
@@ -340,8 +354,8 @@ public class BlockEditGUI {
     private static void handleClickEventSort(int x, int y) {
         if (isWithinButtonRange(x, y, 544, 819, 0, 50)) {
             if (sortType == "Name") {
-                sortType = "Stack";
-            } else if (sortType == "Stack") {
+                sortType = "Stackable";
+            } else if (sortType == "Stackable") {
                     sortType = "Dimension";
             } else if (sortType == "Dimension") {
                 sortType = "Hardness";
